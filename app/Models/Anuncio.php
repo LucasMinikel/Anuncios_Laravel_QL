44c641fdf8333daaadd03c705c5ed49dcae6886c
Lucas Minikel <lucasminikel.m@gmail.com>
2022-01-27 19:39:19 -0300

    Criando anúncio e usuário

--- app/Models/Anuncio.php
+++ app/Models/Anuncio.php
@@ -4,17 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Anuncio extends Model
 {
     use HasFactory;
 
-    public function tags()
+    public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class);
     }
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
